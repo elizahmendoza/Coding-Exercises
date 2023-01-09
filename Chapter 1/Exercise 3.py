7f4f436b59e9e750b1c25a427cc10d91af45ea86
elizahmendoza <116435425+elizahmendoza@users.noreply.github.com>
2023-01-09 22:30:41 +0400

    Delete Chapter 1 directory

--- Chapter 1/Exercise 3.py
+++
@@ -1,4 +0,0 @@
-import datetime #this code imports the date and time
-now = datetime.datetime.now() #the variable 'now' has the value of the current date or time
-print("Current date and time: ") #this code prints the string "current date and time"
-print(now.strftime("%Y-%m-%d %H:%M:%S")) #this program displays the current date and time.
