65903d0be5085f2e7196054eb4d43bf3ded4e460
elizahmendoza <116435425+elizahmendoza@users.noreply.github.com>
2023-01-09 22:31:04 +0400

    Add files via upload

---
+++ Chapter 1/Exercise 5.py
@@ -0,0 +1,4 @@
+PI = 3.14 #This code shows the value of Pi
+r = float(input("Enter the radius of a circle:")) #This code asks the user for the radius of the circle by printing the string "Enter the radius of the circle"
+area = PI * r * r #this code calculates the area by multiplying the Pi by the radius
+print("Area of a circle = %.2f" %area) #this code accepts the radius of a circle from the user and compute the area.
